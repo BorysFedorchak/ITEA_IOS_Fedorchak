a6e652ebe20c99333953ba148ce3d85a89afb5bb
borys <ifedorchac@gmail.com>
2023-07-28 17:04:24 +0300

    Home work

--- ITEA.swiftpm/iAmOlder.playground/Contents.swift
+++ ITEA.swiftpm/iAmOlder.playground/Contents.swift
@@ -1,3 +1,9 @@
-import UIKit
+func iAmOlder(_ number1: Int, _ number2: Int) -> Bool {
+    return number1 > number2
+}
+let number1 = 3
+let number2 = 2
+let result = iAmOlder(number1,number2)
+print(result)
 
-var greeting = "Hello, playground"
+
---
+++ ITEA.swiftpm/yearOfBirth.playground/Contents.swift
@@ -0,0 +1,6 @@
+func yearOfBirth(age: Int, CurrentYear : Int) {
+    let BirthYear = CurrentYear - age
+    print (BirthYear)
+}
+let age = 16
+let year1 = 2023
